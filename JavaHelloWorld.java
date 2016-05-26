d74b168245e63e61752a13041afae0e7886ff840
Steven Lawrence Hunter <slh777@gmail.com>
2016-05-26 13:33:28 -0700

    added another line of output

--- JavaHelloWorld.java
+++ JavaHelloWorld.java
@@ -3,5 +3,6 @@ public class JavaHelloWorld{
 	public static void main( String[] args ){
 		System.out.println( "this works" );
 		System.out.println( "helloworld, btw" );
+		System.out.println ( "the time is now " + new java.util.Date() );
 	}
 }
